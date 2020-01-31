9244646f6efa5f3111ad9a3499844d44c5a25565
telco2011 <davidlopez.david@gmail.com>
2020-01-31 17:45:06 +0100

    build(cz): update commitizen types descriptions

--- .cz-config.js
+++ .cz-config.js
@@ -1,21 +1,56 @@
 module.exports = {
-  types: [
-    { value: 'build', name: 'build: Changes that affect the build system or external dependencies (example scopes: npm)' },
-    { value: 'feat', name: 'feat: A new feature' },
-    { value: 'fix', name: 'fix: A bug fix' },
-    { value: 'docs', name: 'docs: Documentation only changes' },
+  types: [{
+      value: 'build',
+      name: 'build:    Changes that affect the build system or external dependencies (example scopes: npm)'
+    },
+    {
+      value: 'feat',
+      name: 'feat:     A new feature'
+    },
+    {
+      value: 'fix',
+      name: 'fix:      A bug fix'
+    },
+    {
+      value: 'docs',
+      name: 'docs:     Documentation only changes'
+    },
     {
       value: 'style',
-      name: 'style: Changes that do not affect the meaning of the code\n(white-space, formatting, missing semi-colons, etc)',
+      name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
     },
-    { value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature' },
-    { value: 'perf', name: 'perf: A code change that improves performance' },
-    { value: 'test', name: 'test: Adding missing tests' },
-    { value: 'chore', name: 'chore: Changes to the build process or auxiliary tools\nand libraries such as documentation generation' },
-    { value: 'revert', name: 'revert: Revert to a commit' },
-    { value: 'WIP', name: 'WIP: Work in progress' },
+    {
+      value: 'refactor',
+      name: 'refactor: A code change that neither fixes a bug nor adds a feature'
+    },
+    {
+      value: 'perf',
+      name: 'perf:     A code change that improves performance'
+    },
+    {
+      value: 'test',
+      name: 'test:     Adding missing tests'
+    },
+    {
+      value: 'chore',
+      name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'
+    },
+    {
+      value: 'revert',
+      name: 'revert:   Revert to a commit'
+    },
+    {
+      value: 'WIP',
+      name: 'WIP:      Work in progress'
+    },
   ],
-  scopes: [{ name: 'axios' }, { name: 'decorator' }, { name: 'changelog' }],
+  scopes: [{
+    name: 'axios'
+  }, {
+    name: 'decorator'
+  }, {
+    name: 'changelog'
+  }],
   messages: {
     type: "Select the type of change that you're committing:",
     scope: '\nDenote the SCOPE of this change (optional):',
